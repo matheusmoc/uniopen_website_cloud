f9bc53610e709af733f1a3c9ffcc7d1dd3edc4f0
matheusmoc <matheusandrade009@gmail.com>
2024-06-20 23:02:07 -0300

    feat: fix scroll

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,17 @@ import Relation from './components/Relation'
 
 const App = () => {
   return (
-    <main className='bg-black'>
-      <Navbar />
-      <Hero />
-      <Highlights />
-      <Relation />
-    </main>
+    <div
+      className="bg-[rgb(36,36,36)] text-white h-screen snap-mandatory snap-y overflow-y-scroll overflow-x-hidden z-0
+      scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#6e00a5]/80"
+      >
+      <main className='bg-black'>
+        <Navbar />
+        <Hero />
+        <Highlights />
+        <Relation />
+      </main>
+    </div>
   )
 }
 
